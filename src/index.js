701ba5827a724b23946b686d992715e768e714c5
snehal1791 <sparate@my.harrisburgu.edu>
2017-11-19 01:37:34 -0500

    Updated css files path

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import './styles/index.css'
+import '../styles/index.css'
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
