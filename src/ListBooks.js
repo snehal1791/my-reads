08e048814b47db624b68a098200fbb7e82fad9f8
Snehal Parate <sparate@my.harrisburgu.edu>
2017-11-21 20:24:34 -0500

    Update ListBooks.js

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -11,64 +11,67 @@ class ListBooks extends Component {
 
   render() {
     const { books, onShelfChange } = this.props;
-	// categorize books based on their shelf
-	let currentlyReading = books.filter(book => book.shelf === 'currentlyReading');
+    // categorize books based on their shelf
+    let currentlyReading = books.filter(book => book.shelf === 'currentlyReading');
     let wantToRead = books.filter(book => book.shelf === 'wantToRead');
     let read = books.filter(book => book.shelf === 'read');        
-	// Return the DOM
+    // Return the DOM
     return (
       	<div className="list-books">
             <div className="list-books-title">
               	<h1>My Library</h1>
-				<span>Udacity React Nanodegree - Project 1</span>
+		<span>Udacity React Nanodegree - Project 1</span>
             </div>
             <div className="list-books-content">
-				<div>
+		<div>
                   <div className="bookshelf">
                     <h2 className="bookshelf-title">Currently Reading ({currentlyReading.length})</h2>
                     <div className="bookshelf-books">
                     	<ol className="books-grid">
-      						{currentlyReading.map((book) =>
+      			{currentlyReading.map((book) =>
                              /* Component that contains books in a bookshelf */
-      							<BookShelf
-									key={book.id}
-      								book={book}
-      								shelf={book.shelf}
-									onShelfChange={onShelfChange}/>
-      						)}
-      					</ol>
-      				</div>
+      				<BookShelf
+					key={book.id}
+      					book={book}
+      					shelf={book.shelf}
+					onShelfChange={onShelfChange}
+	  			/>
+      			)}
+      			</ol>
+      		     </div>
                   </div>
                   <div className="bookshelf">
                     <h2 className="bookshelf-title">Want to Read ({wantToRead.length})</h2>
                     <div className="bookshelf-books">
                       <ol className="books-grid">
                         {wantToRead.map((book) =>
-      							<BookShelf 
-                         			key={book.id}
-      								book={book}
-      								shelf={book.shelf}
-									onShelfChange={onShelfChange}/>
-      						)}
+      				<BookShelf 
+                         		key={book.id}
+      					book={book}
+      					shelf={book.shelf}
+					onShelfChange={onShelfChange}
+				/>
+      			)}
                       </ol>
                     </div>
                   </div>
-				<div className="bookshelf">
+		  <div className="bookshelf">
                     <h2 className="bookshelf-title">Read ({read.length})</h2>
                     <div className="bookshelf-books">
                       <ol className="books-grid">
                         {read.map((book) =>
-      							<BookShelf 
-                         			key={book.id}
-      								book={book}
-      								shelf={book.shelf}
-									onShelfChange={onShelfChange}/>
-      						)}
+      				<BookShelf 
+                         		key={book.id}
+      					book={book}
+      					shelf={book.shelf}
+					onShelfChange={onShelfChange}
+				/>
+      			)}
                       </ol>
                     </div>
                   </div>
-				</div>
-      		</div>
+		</div>
+      	    </div>
             <div className="open-search">
               <Link to="/search" className="add-book">Add a book</Link>
             </div>
